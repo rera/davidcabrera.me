e11dd727ff9d2a009aec7f80b4916c67a99ef04c
David Cabrera <david@rerainc.com>
2019-05-03 13:38:08 -0400

    style teaser

--- src/components/teaser.js
+++ src/components/teaser.js
@@ -5,7 +5,7 @@ class Teaser extends React.Component {
     return (
 			<section class="teaser">
 				<header>{this.props.header}</header>
-				<a href={this.props.link}>{this.props.linkText}</a>
+				<a className="button white tall" href={this.props.link}>{this.props.linkText}</a>
 			</section>
 		)
   }
