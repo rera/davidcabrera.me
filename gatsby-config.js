ccfe3c23c57c8831e01b13517a086d3052dc8918
David Cabrera <david@rerainc.com>
2019-05-20 08:45:40 -0400

    update bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     company: 'Rera Inc.',
     title: 'Full Stack Developer',
     location: 'Jupiter, Florida',
-    bio: 'Creative full-stack developer with a passion for user experience and 15+ years of experience in web and ecommerce for diverse verticals. Dynamic hands-on leader with a skill set that spans all aspects of digital experience, acquisition and engagement with expertise in design, development, and data management. Advocate of agile practices and rapid iterative prototyping.',
+    bio: 'Creative full-stack developer and hands-on leader with a passion for user experience and 15+ year career building web and ecommerce solutions for diverse verticals. Dynamic skill set that spans digital experience, acquisition, and engagement with expertise in design, development, and data management. Advocate of agile practices and rapid iterative prototyping.',
     social: [
       {
         platform: 'LinkedIn',
